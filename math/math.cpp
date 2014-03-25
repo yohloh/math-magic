0929f73b019fb665f96b17b19340240aaafd7ba7
wenqinYe <wenqinYe@users.noreply.github.com>
2014-03-25 13:50:39 -0400

    add math.cpp

---
+++ math/math.cpp
@@ -0,0 +1,18 @@
+class Math{
+
+  template <typename t> add(a, b){
+    return a + b;
+  }
+  
+  template <typename t> subract(a, b){
+    return a - b;
+  }
+  
+  template <typename t> divide(a, b){
+    return a / b;
+  }
+  
+  remplate <typename t> multiply(a, b){
+    return a * b;
+  }
+}
