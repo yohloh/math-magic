47197f3c1a3b688f13c8d99cba8067a391dfbc1e
wenqinYe <wenqinYe@users.noreply.github.com>
2014-03-25 13:46:48 -0400

    create algebra.cpp

---
+++ algebra.cpp
@@ -0,0 +1,12 @@
+/*
+Takes in two points and finds the slope from the 
+two points. In otherwords, it retunrs the slope of the interval between two points.
+
+Returns the slope for a line based on the 2 points given.
+*/
+template <typename t> findSlope(x1, y1, x1, x2){
+        deltaY = y1 - y2;
+        deltaX = x1 - x2;
+        
+        return deltaX / deltaY
+}
